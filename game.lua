6ed5ec3d181dfa03cb31a74afad43a571778a30d
Rene Sebastian Pucheta <sebastian.pucheta@gmail.com>
2017-10-20 01:10:28 -0300

    Spider above Ants.

--- game.lua
+++ game.lua
@@ -37,11 +37,13 @@ function getNearbyAnt( pCenter, pObjects, pRange, pDebug )
 
     for i=1,#pObjects do
         if pObjects[i] ~= nil then
-            if pObjects[i].x >= left and pObjects[i].x <= right then
-                if pObjects[i].y >= top and pObjects[i].y <= bottom then
-                    result[#result+1] = pObjects[i]
-                end
-            end
+        	if pObjects[i].fsm.current ~= 'dying' then
+	            if pObjects[i].x >= left and pObjects[i].x <= right then
+	                if pObjects[i].y >= top and pObjects[i].y <= bottom then
+	                    result[#result+1] = pObjects[i]
+	                end
+	            end
+	        end
         end
     end
 
@@ -63,141 +65,6 @@ function scene:create( event )
 	background.y = display.contentHeight / 2
 
 	sceneGroup:insert(background)
-
-	-- loading spiders
-	local sequenceData =
-	{
-	    name="walking",
-	    frames= { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }, -- frame indexes of animation, in image sheet
-	    time = 1000,
-	    loopCount = 0        -- Optional ; default is 0
-	}
-
-	local sheetData = { width=120, height=148, numFrames=19, sheetContentWidth=480, sheetContentHeight=740 }
-	local imageSheet = graphics.newImageSheet( "spider_crawl.png", sheetData )
-
-	for i = 1, 3 do
-		local spider = display.newSprite( imageSheet, sequenceData )
-
-		spider:scale(0.25, 0.25)
-		spider.x = math.random(display.contentWidth)
-		spider.y = math.random(display.contentHeight)
-		spider:setFillColor(255, 255, 255, 255)
-
-		spider.fsm = machine.create({
-		  initial = 'idle',
-		  events = {
-		    { name = 'roam',  from = {'idle', 'roaming'},  to = 'roaming' },
-		    { name = 'stay',  from = 'roaming',  to = 'idle' },
-		    { name = 'attack',  from = {'waiting', 'roaming'},  to = 'scarejump' },
-		    { name = 'wait',  from = 'roaming',  to = 'waiting' }
-		  },
-		  callbacks = {
-		    onroam =    function(self, event, from, to, idx)
-		    	local angle = math.random(360)
-
-		    	if (spiders[idx].x > display.contentWidth) then angle = 360 - angle end
-		    	if (spiders[idx].y > display.contentHeight) then angle = 90 - angle end
-		    	if (spiders[idx].x < 0) then angle = 360 - angle end
-		    	if (spiders[idx].y < 0) then angle = 90 - angle end
-				local angleX = spiders[idx].x + 50 * math.cos( math.rad( angle - 90 ) )
-				local angleY = spiders[idx].y + 50 * math.sin( math.rad( angle - 90 ) )
-
-				angleX = (angleX < 0) and 0 or angleX
-				angleX = (angleX > display.contentWidth) and display.contentWidth or angleX
-				angleY = (angleY < 0) and 0 or angleY
-				angleY = (angleY > display.contentHeight) and display.contentHeight or angleY
-
-				spiders[idx]:setFillColor(255, 255, 255, 255)
-				spiders[idx].color = "none"
-
-				spiders[idx]:play()
-				spiders[idx].rotation = 0
-		    	spiders[idx]:rotate(angle)
-		        transition.to ( spiders[idx], { time=math.random(1500, 5000), x=angleX , y=angleY , onComplete=function() 
-		        	if (math.random(5) == 5) then
-		        		spiders[idx].fsm:stay(idx)
-		        	else
-		        		spiders[idx].fsm:roam(idx)
-		        	end
-		        end})
-			end,
-		    onidle =    function(self, event, from, to, idx)
-		    	spiders[idx]:pause()
-
-		    	timer.performWithDelay(math.random(1500, 4000), function()
-					spiders[idx].fsm:roam(idx)
-		    	end)
-
-				local nearbyAnt = getNearbyAnt(spiders[idx], ants, 100)[1]
-				if (nearbyAnt ~= nil) then
-					print(nearbyAnt.index)
-				end
-		    end,
-		    onattack =    function(self, event, from, to)
-		    end,
-		    onwaiting = function (self, event, from, to, idx)
-		    	timer.performWithDelay(350, function() 
-		    		-- spiders[idx].fsm:attack(idx) 
-		    	end)
-		    end,
-		    onscarejump =    function(self, event, from, to, idx) 
-	    		audio.play( spider_attacking_sound )
-
-	    		timer.performWithDelay(100, function()
-			    	-- background.fill.effect = "filter.blurGaussian"
-					
-					-- transition.to( background.fill.effect.horizontal, { time=300, blurSize=30, transition=easing.outCirc } )
-					-- transition.to( background.fill.effect.vertical, { time=300, blurSize=30, transition=easing.outCirc } )			
-
-					for i, v in ipairs(spiders) do 
-						if (i ~= idx) then
-							-- v.fill.effect = "filter.blurGaussian"
-							--transition.to( v.fill.effect.horizontal, { time=1000, blurSize=30, transition=easing.outCirc } )
-							--transition.to( v.fill.effect.vertical, { time=1000, blurSize=30, transition=easing.outCirc } )			
-						end
-					end
-
-					transition.to( spiders[idx], { y=spiders[idx].y - 60, time=100, transition=easing.inExpo, onComplete=function(event)
-						transition.scaleTo( spiders[idx], { xScale=20, yScale=20, time=250, transition=easing.inExpo } )		    
-						transition.to( spiders[idx], { y=spiders[idx].y + 250, time=150, transition=easing.inExpo, onComplete=function(event)
-						end } )		    
-					end } )		    
-					
-
-					spiders[idx]:toFront()	    			
-	    		end)
-			end
-		  }
-		})
-		spider.timer = system.getTimer()
-		spider.color = "none"
-		spider.enterFrame = function ( self )
-			if (self.fsm.current == "idle") then
-				if (system.getTimer() - self.timer > 75) then
-					if (self.color == "none") then
-						self:setFillColor(0, 0, 255, 255)
-						self.color = "blue"
-					else
-						self:setFillColor(255, 255, 255, 255)
-						self.color = "none"
-					end
-					spider.timer = system.getTimer()
-				end
-			end
-		end
-		spider.index = table.getn(spiders) + 1
-		Runtime:addEventListener("enterFrame", spider)
-		spider:addEventListener("touch", function(event)
-		  if(event.phase == "ended") then
-		    spider.fsm:attack(spider.index)
-		  end
-		end)
-		spiders[table.getn(spiders) + 1] = spider
-		spider.fsm:roam(table.getn(spiders))	
-	
-		sceneGroup:insert(spider)
-	end
 
 	-- loading ants
 	local sequenceData =
@@ -309,6 +176,151 @@ function scene:create( event )
 		ant.fsm:roam(table.getn(ants))	
 	
 		sceneGroup:insert(ant)
+	end
+
+	-- loading spiders
+	local sequenceData =
+	{
+	    name="walking",
+	    frames= { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }, -- frame indexes of animation, in image sheet
+	    time = 1000,
+	    loopCount = 0        -- Optional ; default is 0
+	}
+
+	local sheetData = { width=120, height=148, numFrames=19, sheetContentWidth=480, sheetContentHeight=740 }
+	local imageSheet = graphics.newImageSheet( "spider_crawl.png", sheetData )
+
+	for i = 1, 3 do
+		local spider = display.newSprite( imageSheet, sequenceData )
+
+		spider:scale(0.25, 0.25)
+		spider.x = math.random(display.contentWidth)
+		spider.y = math.random(display.contentHeight)
+		spider:setFillColor(255, 255, 255, 255)
+
+		spider.fsm = machine.create({
+		  initial = 'idle',
+		  events = {
+		    { name = 'roam',  from = {'killing', 'idle', 'roaming'},  to = 'roaming' },
+		    { name = 'stay',  from = 'roaming',  to = 'idle' },
+		    { name = 'attack',  from = {'waiting', 'roaming'},  to = 'scarejump' },
+		    { name = 'wait',  from = 'roaming',  to = 'waiting' },
+		    { name = 'kill',  from = 'idle',  to = 'killing' }
+		  },
+		  callbacks = {
+		  	onkill =    function(self, event, from, to, idx, antidx)
+				transition.to ( spiders[idx], { time=350, x=ants[antidx].x, y=ants[antidx].y, transition=easing.outCirc, onComplete=function() 
+					ants[antidx].fsm:die(antidx)
+					spiders[idx].fsm:roam(idx)
+		        end})		  	
+		  	end,
+		    onroam =    function(self, event, from, to, idx)
+		    	local angle = math.random(360)
+
+		    	if (spiders[idx].x > display.contentWidth) then angle = 360 - angle end
+		    	if (spiders[idx].y > display.contentHeight) then angle = 90 - angle end
+		    	if (spiders[idx].x < 0) then angle = 360 - angle end
+		    	if (spiders[idx].y < 0) then angle = 90 - angle end
+				local angleX = spiders[idx].x + 50 * math.cos( math.rad( angle - 90 ) )
+				local angleY = spiders[idx].y + 50 * math.sin( math.rad( angle - 90 ) )
+
+				angleX = (angleX < 0) and 0 or angleX
+				angleX = (angleX > display.contentWidth) and display.contentWidth or angleX
+				angleY = (angleY < 0) and 0 or angleY
+				angleY = (angleY > display.contentHeight) and display.contentHeight or angleY
+
+				spiders[idx]:setFillColor(255, 255, 255, 255)
+				spiders[idx].color = "none"
+
+				spiders[idx]:play()
+				spiders[idx].rotation = 0
+		    	spiders[idx]:rotate(angle)
+		        transition.to ( spiders[idx], { time=math.random(1500, 5000), x=angleX , y=angleY , onComplete=function() 
+		        	if (math.random(5) == 5) then
+		        		spiders[idx].fsm:stay(idx)
+		        	else
+		        		spiders[idx].fsm:roam(idx)
+		        	end
+		        end})
+			end,
+		    onidle =    function(self, event, from, to, idx)
+		    	spiders[idx]:pause()
+
+				local nearbyAnt = getNearbyAnt(spiders[idx], ants, 100)[1]
+				if (nearbyAnt ~= nil) then
+					print("killing: " .. nearbyAnt.index)
+			    	timer.performWithDelay(math.random(1500, 4000), function()
+						spiders[idx].fsm:kill(idx, nearbyAnt.index)
+			    	end)
+				else					
+			    	timer.performWithDelay(math.random(1500, 4000), function()
+						spiders[idx].fsm:roam(idx)
+			    	end)					
+				end
+		    end,
+		    onattack =    function(self, event, from, to)
+		    end,
+		    onwaiting = function (self, event, from, to, idx)
+		    	timer.performWithDelay(350, function() 
+		    		-- spiders[idx].fsm:attack(idx) 
+		    	end)
+		    end,
+		    onscarejump =    function(self, event, from, to, idx) 
+	    		audio.play( spider_attacking_sound )
+
+	    		timer.performWithDelay(100, function()
+			    	-- background.fill.effect = "filter.blurGaussian"
+					
+					-- transition.to( background.fill.effect.horizontal, { time=300, blurSize=30, transition=easing.outCirc } )
+					-- transition.to( background.fill.effect.vertical, { time=300, blurSize=30, transition=easing.outCirc } )			
+
+					for i, v in ipairs(spiders) do 
+						if (i ~= idx) then
+							-- v.fill.effect = "filter.blurGaussian"
+							--transition.to( v.fill.effect.horizontal, { time=1000, blurSize=30, transition=easing.outCirc } )
+							--transition.to( v.fill.effect.vertical, { time=1000, blurSize=30, transition=easing.outCirc } )			
+						end
+					end
+
+					transition.to( spiders[idx], { y=spiders[idx].y - 60, time=100, transition=easing.inExpo, onComplete=function(event)
+						transition.scaleTo( spiders[idx], { xScale=20, yScale=20, time=250, transition=easing.inExpo } )		    
+						transition.to( spiders[idx], { y=spiders[idx].y + 250, time=150, transition=easing.inExpo, onComplete=function(event)
+						end } )		    
+					end } )		    
+					
+
+					spiders[idx]:toFront()	    			
+	    		end)
+			end
+		  }
+		})
+		spider.timer = system.getTimer()
+		spider.color = "none"
+		spider.enterFrame = function ( self )
+			if (self.fsm.current == "idle") then
+				if (system.getTimer() - self.timer > 75) then
+					if (self.color == "none") then
+						self:setFillColor(0, 0, 255, 255)
+						self.color = "blue"
+					else
+						self:setFillColor(255, 255, 255, 255)
+						self.color = "none"
+					end
+					spider.timer = system.getTimer()
+				end
+			end
+		end
+		spider.index = table.getn(spiders) + 1
+		Runtime:addEventListener("enterFrame", spider)
+		spider:addEventListener("touch", function(event)
+		  if(event.phase == "ended") then
+		    spider.fsm:attack(spider.index)
+		  end
+		end)
+		spiders[table.getn(spiders) + 1] = spider
+		spider.fsm:roam(table.getn(spiders))	
+	
+		sceneGroup:insert(spider)
 	end
 
 	local rect1 = display.newRect(display.contentWidth / 4, display.contentHeight / 2, 10, display.contentHeight)
